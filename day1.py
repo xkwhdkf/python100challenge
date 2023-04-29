d37c1d8cf719df5225c7cf792afc5cd8bdbfd759
Kim jin hyang <73262330+xkwhdkf@users.noreply.github.com>
2023-04-29 20:43:03 +0900

    day 1

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("Hello");
+street = input("what's the name of the city you grow up in?\n");
+input("what is your name?")
+pet = input("what's your pet's name?\n");
+print("your band name could be" + street + " " + pet)
